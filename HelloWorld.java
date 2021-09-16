4e72503caa2b680ce0ea62dc90a40f70a252fb81
Ширшов Георгий Вадимович <g.shirshov@univ.priv>
2021-09-16 14:48:36 +0300

    Update file HelloWorld.java after picking up the changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,11 @@
 public class Hello{
 	public static void main(String[] args){
+<<<<<<< HEAD
 		System.out.println("Hello, world!1");
 		System.out.println("New Feature!");	
+=======
+		System.out.println("Hello, world!\n");
+		System.out.println("New Feature!");
+>>>>>>> 499d390 (Update file HelloWorld.java on branch new-config)
 	}
 }
